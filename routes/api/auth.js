31861b1d2d2709333847e8fb799b5707dfcb516d
Mnargh <tom.j.v.brandon@gmail.com>
2020-01-24 15:51:01 +0000

    User authentication and login route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
 const auth = require("../../middleware/auth");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const { check, validationResult } = require("express-validator");
 
 const User = require("../../models/User");
 // @route           GET api/auth
@@ -19,6 +23,71 @@ router.get("/", auth, async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
+
+// @route           POST api/auth
+// @description     Authenticate user and get token
+// @access          public
+router.post(
+  "/",
+  [
+    check("email", "Please include a valid email").isEmail(),
+    check("password", "Password is required").exists()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      // See if user exists, if not send error
+      let user = await User.findOne({ email });
+      // check to see if a user, if not, send an error message
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid credentials" }] });
+      }
+
+      // compare takes first param password, which is what plaintext password user has entered
+      // second is encrypted password we get from user we got from rquesting to the db to get the user
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      // check to see if there is a match
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid credentials" }] });
+        // for security reasons, need to keep message the same if user not found or password wrong for security reasons
+      }
+
+      // Return jsonwebtoken
+      // get payload
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      // sign token, passing in payload and secret
+      // required to be logged in straight away
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (error, token) => {
+          if (error) throw error;
+          res.json({ token });
+        }
+      );
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 // always need to export the router
 module.exports = router;
