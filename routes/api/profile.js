2b86d5b634bbb4c8cac08a08cd7208cc66648ea6
Mnargh <tom.j.v.brandon@gmail.com>
2020-01-27 23:22:46 +0000

    mid adding profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,19 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
 
-// @route           GET api/profile
-// @description     auth route
-// @access          public 
-router.get('/', (req, res) => res.send('Profile route'));
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+// @route           GET api/profile/me
+// @description     Get current users profile
+// @access          private - getting profile by user id in the token, so they have to use the token
+router.get("/", auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: res.user.id })
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
 
 module.exports = router;
