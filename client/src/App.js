516c9267a208ad6c5afec021202874a30d821a50
Mnargh <tom.j.v.brandon@gmail.com>
2020-04-13 23:09:55 +0100

    added react router setup. Added register form and usestate hook

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import "./App.css";
+
+const App = () => (
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Route exact path="/" component={Landing} />
+      <section className="container"></section>
+      <Switch>
+        <Route exact path="/register" component={Register}></Route>
+        <Route exact path="/login" component={Login}></Route>
+      </Switch>
+    </Fragment>
+  </Router>
+);
+
+export default App;
