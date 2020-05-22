8fb672b540d119a6315e11ddfb0cce81f340b432
Mnargh <tom.j.v.brandon@gmail.com>
2020-05-22 13:09:18 +0100

    Added logout and navbar links

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -7,6 +7,7 @@ import {
   AUTH_ERROR,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
+  LOGOUT,
 } from "./types";
 import setAuthToken from "../utils/setAuthToken";
 
@@ -91,4 +92,9 @@ export const login = (email, password) => async (dispatch) => {
       type: LOGIN_FAIL,
     });
   }
+};
+
+// Logout User/ Clear Profile
+export const logout = () => (dispatch) => {
+  dispatch({ type: LOGOUT });
 };
