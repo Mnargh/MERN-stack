868a0a1d7da035b97593373fa6bcdb3e41a325ec
Tom Brandon <tom.brandon@rbs.co.uk>
2020-11-01 22:50:48 +0000

    Sourcing logins from env variables

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,29 +1,27 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const gravatar = require("gravatar");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const config = require("config");
-const { check, validationResult } = require("express-validator");
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
 
 // importing User model
-const User = require("../../models/User");
+const User = require('../../models/User');
 
 // @route           POST api/users
 // @description     Register user
 // @access          public
 // (if need a token to access specific route) will creating authenticaition later
 router.post(
-  "/",
+  '/',
   [
-    check("name", "Name is required")
-      .not()
-      .isEmpty(),
-    check("email", "Please include a valid email").isEmail(),
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
     check(
-      "password",
-      "Please enter a password with 6 or more characters"
-    ).isLength({ min: 6 })
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 }),
   ],
   async (req, res) => {
     const errors = validationResult(req);
@@ -39,20 +37,20 @@ router.post(
       if (user) {
         return res
           .status(400)
-          .json({ errors: [{ msg: "User already exists" }] });
+          .json({ errors: [{ msg: 'User already exists' }] });
       }
       // Get user's gravitar
       const avatar = gravatar.url(email, {
-        s: "200", // size
-        r: "pg", // rating
-        d: "mm" // default image
+        s: '200', // size
+        r: 'pg', // rating
+        d: 'mm', // default image
       });
 
       user = new User({
         name,
         email,
         avatar,
-        password
+        password,
       });
 
       // Encrypt password
@@ -66,15 +64,15 @@ router.post(
       // get payload
       const payload = {
         user: {
-          id: user.id
-        }
+          id: user.id,
+        },
       };
 
       // sign token, passing in payload and secret
       // required to be logged in straight away
       jwt.sign(
         payload,
-        config.get("jwtSecret"),
+        process.env.JWT_SECRET,
         { expiresIn: 360000 },
         (error, token) => {
           if (error) throw error;
@@ -83,7 +81,7 @@ router.post(
       );
     } catch (error) {
       console.error(error.message);
-      res.status(500).send("Server Error");
+      res.status(500).send('Server Error');
     }
   }
 );
