ab727442bb4b601d3a830d142ace72a4824dd73b
Tom Brandon <tom.brandon@rbs.co.uk>
2020-12-23 17:25:40 +0000

    Added removal of posts to delete profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -7,6 +7,7 @@ const { check, validationResult } = require('express-validator');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const Post = require('../../models/Post')
 
 // @route           GET api/profile/me
 // @description     Get current users profile
@@ -156,8 +157,9 @@ router.get('/user/:user_id', async (req, res) => {
 
 router.delete('/', auth, async (req, res) => {
   try {
-    // todo - remove users posts
-
+    // remove users posts
+    await Post.deleteMany({ user: req.user.id })
+    
     // remove profile
     await Profile.findOneAndRemove({ user: req.user.id });
 
