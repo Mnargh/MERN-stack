feaf8089055808f8d87e8aa175ab2de12777108a
Tom Brandon <tom.brandon@rbs.co.uk>
2020-12-27 22:22:24 +0000

    added profile top component

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -4,13 +4,15 @@ import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import Spinner from "../layout/Spinner"
 import { getProfileByID } from '../../actions/profile'
+import ProfileTop from './ProfileTop'
+import ProfileAbout from './ProfileAbout'
 import auth from '../../reducers/auth'
 
 const Profile = ({ getProfileByID, profile: { profile, loading }, auth, match }) => {
 
   useEffect(() => {
     getProfileByID(match.params.id);
-  }, [getProfileByID])
+  }, [getProfileByID, match.params.id])
 
   return (
     <Fragment>
@@ -19,8 +21,12 @@ const Profile = ({ getProfileByID, profile: { profile, loading }, auth, match })
           <Link to="/profiles" className='btn btn-light'>
             Back to Profiles
           </Link>
-          {auth.isAuthenticated && auth.loading === false && auth.user._id === profile.user._id && (<Link to="'/edit-profile" className="btn btn-dark">Edit Profile</Link>)}
-        </Fragment>)}
+          {auth.isAuthenticated && auth.loading === false && auth.user._id === profile.user._id && (<Link to="/edit-profile" className="btn btn-dark">Edit Profile</Link>)}
+          <div class="profile-grid my-1">
+            <ProfileTop profile={profile} />
+          </div>
+        </Fragment>
+      )}
     </Fragment>
   )
 }
