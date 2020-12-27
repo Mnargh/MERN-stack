42ca66e908b8cfd642c3261379b064a8d8b9b94d
Tom Brandon <tom.brandon@rbs.co.uk>
2020-12-27 23:35:27 +0000

    Added profile about section

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -24,6 +24,7 @@ const Profile = ({ getProfileByID, profile: { profile, loading }, auth, match })
           {auth.isAuthenticated && auth.loading === false && auth.user._id === profile.user._id && (<Link to="/edit-profile" className="btn btn-dark">Edit Profile</Link>)}
           <div class="profile-grid my-1">
             <ProfileTop profile={profile} />
+            <ProfileAbout profile={profile} />
           </div>
         </Fragment>
       )}
