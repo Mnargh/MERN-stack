5b132e706222021a4e55b91600ebe6ef09e3afe9
Tom Brandon <tom.brandon@rbs.co.uk>
2020-12-29 19:13:09 +0000

    added view profile link to dashboard. Fixed other console warnings

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -28,7 +28,7 @@ const Dashboard = ({
       </p>
       {profile !== null ? (
         <Fragment>
-          <DashboardActions />
+          <DashboardActions profile={profile} />
           <Experience experience={profile.experience} />
           <Education education={profile.education} />
           <div className="my-2">
--- client/src/components/profile/ProfileGithub.js
+++ client/src/components/profile/ProfileGithub.js
@@ -8,44 +8,46 @@ const ProfileGithub = ({ username, getGithubRepos, repos }) => {
 
   useEffect(() => {
     getGithubRepos(username)
-  }, [getGithubRepos])
+  }, [getGithubRepos, username])
 
 
   return <div className="profile-github">
     <h2 className="text-primary my-1">Github Repos</h2>
-    { repos === null ? <Spinner/> : (repos.map(repo => (
-      <div key={repo._id} className='repo bg-white p-1 my-1'>
-        <div>
-          <h4>
-            <a href={repo.html_url}
-              target='_blank' 
-              rel='noopener noreferrer'>
-              {repo.name}</a>
-          </h4>
-          <p>{repo.description}</p>
+    { repos === null ? <Spinner/> : (
+      repos.map(repo => (
+        <div key={repo.id} className='repo bg-white p-1 my-1'>
+          <div>
+            <h4>
+              <a href={repo.html_url}
+                target='_blank' 
+                rel='noopener noreferrer'>
+                {repo.name}</a>
+            </h4>
+            <p>{repo.description}</p>
+          </div>
+          <div>
+            <ul>
+              <li className="badge badge-primary">
+                Stars: {repo.stargazers_count}
+              </li>
+              <li className="badge badge-dark">
+                Watchers: {repo.watchers_count}
+              </li>
+              <li className="badge badge-light">
+                Forks: {repo.forks_count}
+              </li>
+            </ul>
+          </div>
         </div>
-        <div>
-          <ul>
-            <li className="badge badge-primary">
-              Stars: {repo.stargazers_count}
-            </li>
-            <li className="badge badge-dark">
-              Watchers: {repo.watchers_count}
-            </li>
-            <li className="badge badge-light">
-              Forks: {repo.forks_count}
-            </li>
-          </ul>
-        </div>
-      </div>
-    )))}
+      )
+    ))}
   </div>
 }
 
 ProfileGithub.propTypes = {
   getGithubRepos: PropTypes.func.isRequired,
-  repo: PropTypes.object.isRequired,
-  username: PropTypes.object.isRequired,
+  repos: PropTypes.array.isRequired,
+  username: PropTypes.string.isRequired,
 }
 
 const mapStateToProps = state => ({
