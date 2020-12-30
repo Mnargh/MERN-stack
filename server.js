d9b00793f261a6ebff0769c8b74270a6224aa28b
Tom Brandon <tom.brandon@rbs.co.uk>
2020-12-30 12:27:08 +0000

    empty commit

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ app.use('/api/posts', require('./routes/api/posts'));
 
 // serve static assets in production
 if (process.env.NODE_ENV === 'production') {
+
   // set static folder
   app.use(express.static('client/build'));
 
